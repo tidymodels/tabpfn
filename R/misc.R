ef83e55546a4a0d45b8ac1f5beff69b77b9e4a66
Edgar Ruiz <77294576+edgararuiz@users.noreply.github.com>
2026-05-06 12:57:28 -0500

    Adds list_tabpfn_versions

--- R/misc.R
+++ R/misc.R
@@ -157,16 +157,29 @@ is_tab_pfn_installed <- function() {
 }
 
 
-check_model_version <- function(x, call = rlang::caller_env()) {
+#' List available TabPFN model versions
+#'
+#' Returns a character vector of valid model version strings accepted by
+#' [tab_pfn()]'s `version` argument. The available model versions are queried
+#' directly from the currently installed Python `tabpfn` library, not
+#' hard-coded in this package, so results may differ across Python library
+#' versions.
+#' @return A character vector of model version strings.
+#' @export
+list_tabpfn_versions <- function() {
   if (!is_tab_pfn_installed()) {
-    cli::cli_abort(msg_tabpfn_not_available(), call = call)
+    cli::cli_abort(msg_tabpfn_not_available())
   }
 
   py_lib <- reticulate::import("tabpfn")
   builtins <- reticulate::import_builtins()
-  valid_versions <- py_lib$constants$ModelVersion |>
+  py_lib$constants$ModelVersion |>
     builtins$list() |>
     unlist()
+}
+
+check_model_version <- function(x, call = rlang::caller_env()) {
+  valid_versions <- list_tabpfn_versions()
 
   if (!x %in% valid_versions) {
     cli::cli_abort(

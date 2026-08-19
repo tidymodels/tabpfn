9ad8ae0f1de22ef31744ded557db6b4ef79a531c
Max Kuhn <mxkuhn@gmail.com>
2026-08-19 11:09:36 -0400

    univariate example and re-doc

--- R/TabPFN-predict.R
+++ R/TabPFN-predict.R
@@ -28,22 +28,56 @@
 #' `.pred_quantile` column of [hardhat::quantile_pred()] values.
 #'
 #' @examples
-#' # Minimal example for quick execution
-#' car_train <- mtcars[ 1:5,   ]
-#' car_test  <- mtcars[6, -1]
-#'
 #' \dontrun{
-#' # Fit
-#' if (is_tab_pfn_installed() & interactive()) {
-#'  mod <- tab_pfn(mpg ~ cyl + log(drat), car_train)
+#' if (rlang::is_installed(c("MASS", "ggplot2")) &
+#'      is_tab_pfn_installed() &
+#'      interactive()) {
+#'   library(ggplot2)
 #'
-#'  # Predict
-#'  predict(mod, car_test)
-#'  predict(mod, car_test, quantile_levels = c(0.1, 0.5, 0.9))
-#'  augment(mod, car_test)
+#'   in_tr <- seq(1, nrow(mcycle), by = 2)
+#'   mcycle_tr <- MASS::mcycle[in_tr, ]
+#'   mcycle_te <- MASS::mcycle[-in_tr, ]
+#'
+#'   mcycle_grid <-
+#'    dplyr::tibble(times = seq(min(mcycle$times), max(mcycle$times), length.out = 200))
+#'   mcycle_grid$.row <- seq_len(nrow(mcycle_grid))
+#'
+#'   fit <- tab_pfn(accel ~ times, data = mcycle_tr)
+#'
+#'   # ------------------------------------------------------------------------------
+#'   # Predict mean acceleration
+#'
+#'   mean_pred <- augment(fit, mcycle_grid)
+#'
+#'   mean_p <-
+#'    mean_pred |>
+#'    ggplot(aes(times)) +
+#'    geom_point(data = mcycle_te, aes(y = accel), alpha = 1 / 2) +
+#'    geom_line(aes(y = .pred))
+#'
+#'   #------------------------------------------------------------------------------Predict 5 %, 50%
+#'   # Predict 5%, 50%, and 90% quantiles of acceleration
+#'
+#'   q_pred <-
+#'    predict(fit,
+#'            mcycle_grid,
+#'            type = "quantile",
+#'            quantile_levels = c(0.1, 0.5, 0.9))
+#'   q_pred$.row <- seq_len(nrow(q_pred))
+#'
+#'   q_pred_longer <-
+#'    q_pred$.pred_quantile |>
+#'    dplyr::as_tibble() |>
+#'    dplyr::full_join(mcycle_grid, by = ".row") |>
+#'    dplyr::mutate(level = format(.quantile_levels))
+#'
+#'   mean_p +
+#'    geom_line(
+#'      data = q_pred_longer,
+#'      aes(y = .pred_quantile, col = level, group = level)
+#'    )
 #' }
 #' }
-#'
 #' @export
 predict.tab_pfn <- function(
   object,
